92de122459161b74c5f2f2f22b7cbd136e45d2d0
Juraj Zec <juro.zec.1@gmail.com>
2026-03-29 14:20:29 +0200

    updated materialized view order_stats with ordering by total spend nulls last

--- libs/database/src/schema.ts
+++ libs/database/src/schema.ts
@@ -1,5 +1,5 @@
 import { integer, numeric, pgMaterializedView, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core'
-import { countDistinct, eq, max, sql } from 'drizzle-orm'
+import { countDistinct, desc, eq, max, sql } from 'drizzle-orm'
 
 export const users = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -23,15 +23,16 @@ export const orderItems = pgTable('order_items', {
 export const orderStats = pgMaterializedView('order_stats').as((qb) =>
   qb
     .select({
-      customer_id: sql`${users.id}`.as('customer_id'),
+      customerId: sql`${users.id}`.as('customer_id'),
       name: users.name,
-      total_orders: countDistinct(orders.id).as('total_orders'),
-      total_spend: sql`sum(${orderItems.price} * ${orderItems.quantity})`.as('total_spend'),
-      avg_order_value: sql`avg(${orderItems.price}  * ${orderItems.quantity})`.as('avg_order_value'),
-      last_order_at: max(orders.createdAt).as('last_order_at'),
+      totalOrders: countDistinct(orders.id).as('total_orders'),
+      totalSpend: sql`sum(${orderItems.price} * ${orderItems.quantity})`.as('total_spend'),
+      avgOrderValue: sql`avg(${orderItems.price} * ${orderItems.quantity})`.as('avg_order_value'),
+      lastOrderAt: max(orders.createdAt).as('last_order_at'),
     })
     .from(users)
     .leftJoin(orders, eq(orders.customerId, users.id))
     .leftJoin(orderItems, eq(orderItems.orderId, orders.id))
     .groupBy(users.id, users.name)
+    .orderBy((a) => desc(a.totalSpend).append(sql.raw(' nulls last')))
 )
