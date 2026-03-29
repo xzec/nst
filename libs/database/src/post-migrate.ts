c798d0f1895359efe1cacd74ddf0b47476e18c57
Juraj Zec <juro.zec.1@gmail.com>
2026-03-29 15:19:50 +0200

    updated post-migrate script to use pg pool connection and ensure cleanup

--- libs/database/src/post-migrate.ts
+++ libs/database/src/post-migrate.ts
@@ -1,15 +1,20 @@
 import dotenv from 'dotenv'
 import { sql } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 dotenv.config({ path: '../../.env', quiet: true })
 
-const db = drizzle(process.env.DATABASE_URL!)
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
 
-console.log('Running post-migrate script...')
+const db = drizzle(pool)
+
+console.log('post-migrate running...')
 
 await db.execute(
   sql`create unique index if not exists "order_stats_customer_id_unique_idx" on "order_stats" ("customer_id");`
 )
 
 console.log('post-migrate finished successfully.')
+
+await pool.end()
