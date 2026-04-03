1c7cc6700ee21e3e5a03efb6cbfa24379712ba2d
Juraj Zec <juro.zec.1@gmail.com>
2026-04-03 21:54:37 +0200

    refactored ZodValidationPipe to use generic type for schema definition

--- apps/api/src/config/zod-validation.pipe.ts
+++ apps/api/src/config/zod-validation.pipe.ts
@@ -1,8 +1,8 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common'
 import type { ZodType } from 'zod'
 
-export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodType) {}
+export class ZodValidationPipe<T extends ZodType> implements PipeTransform {
+  constructor(private schema: T) {}
 
   transform(value: unknown) {
     try {
